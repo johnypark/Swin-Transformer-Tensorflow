a3c65ec404febe7a5e52034baeda6bcd6b3ba22a
johnypark <parkjohnyc@gmail.com>
2022-05-05 09:52:41 -0400

    Update __init__.py

--- swin_transformer_tensorflow/__init__.py
+++ swin_transformer_tensorflow/__init__.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 ##DATALOADER 
 #from SwinT import configs
 #from SwinT import images
-from swin_transformer_tensorflow import models
+from swin_transformer_tensorflow import model
 from swin_transformer_tensorflow import config
 from swin_transformer_tensorflow import convert_weights
 from swin_transformer_tensorflow import main
